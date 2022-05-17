cdb016e26c1d4490ff9d17816e6d22e0024d13e1
baxy5 <bakos.soda@gmail.com>
2022-05-17 12:44:51 +0200

    message sender done

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,11 @@
+import MessageSender from "./MessageSender";
 import StoryReel from "./StoryReel";
 
 const Feed = () => {
   return (
     <div className="feed flex flex-col justify-center items-center py-8 px-40">
       <StoryReel />
-      {/* MessageSender */}
+      <MessageSender />
     </div>
   );
 };
--- src/components/Story.js
+++ src/components/Story.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Story({ title, Image }) {
   return (
-    <div className="story relative w-32 h-52 shadow-lg mr-3 rounded-xl hover:scale-110 transform transition-all duration-200 ease-in-out cursor-pointer">
-      <img src={Image} className="w-full h-full" />
+    <div className="story relative w-32 h-52 shadow-lg mr-3 hover:scale-110 transform transition-all duration-200 ease-in-out cursor-pointer">
+      <img src={Image} className="w-full h-full rounded-xl" />
       <h4 className="absolute bottom-5 left-5 text-white font-bold ">
         {title}
       </h4>
